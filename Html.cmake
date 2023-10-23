e84744b7b7c6fbe11dfc695ce966768b73199adb
surajy9598 <132045271+surajy9598@users.noreply.github.com>
2023-10-23 13:10:59 +0530

    Add files via upload

---
+++ Html.cmake
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="css.css" />
+    <title>Suraj yadav Landing Page</title>
+  </head>
+  <body>
+    <header>
+      <h1>Octanet Internship</h1>
+      <nav>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Features</a></li>
+          <li><a href="#">Pricing</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+
+    <section class="hero">
+      <h2>Welcome to our Awesome Product</h2>
+      <p>Discover the amazing features that will revolutionize your life!</p>
+      
+      <a href="#" class="cta-button">Get Started</a>
+    </section>
+
+    <footer>
+      <p>
+        &copy; 2023 Your Company. All rights reserved.<br />
+        Owner- Suraj yadav
+        Web Development Internship (Level 1:- Task 1)
+    
+      <p>Email-Id surajy0551@gmail.com</p>
+      <p> Contact 9598912829 </p>
+      </p>
+    </footer>
+  </body>
+</html>
